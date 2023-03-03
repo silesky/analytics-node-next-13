e45a6678efb021f593b6f47ab2608ddb14fa817e
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-03-03 02:54:46 -0600

    wip

---
+++ pages/api/hello.ts
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-anonymous-default-export */
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Analytics } from "@segment/analytics-node";
+import { NextRequest, NextResponse } from "next/server";
+
+export const analytics = new Analytics({
+  writeKey: "DjTUVRhleGaZX31JQpj6XIAaprCIb25W",
+  maxEventsInBatch: 1,
+})
+  .on("error", console.error)
+  .on("http_request", console.log);
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async (req: NextRequest) => {
+  await new Promise((resolve) =>
+    analytics.track(
+      { userId: "edge runtime", event: "hello from edge!" },
+      resolve
+    )
+  );
+  return NextResponse.json({
+    name: `Hello, from ${req.url} I'm now an Edge Function!`,
+  });
+};
